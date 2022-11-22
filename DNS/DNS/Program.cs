24a67f0e84d1b3551d9654a39927af5199625ba4
vmion <111511415+vmion@users.noreply.github.com>
2022-11-22 21:54:48 +0900

    _11_22_
    DNS

---
+++ DNS/DNS/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace DNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry hostEntry = Dns.GetHostEntry("www.google.com");
+            Console.WriteLine(hostEntry.HostName);
+            foreach (IPAddress ip in hostEntry.AddressList)
+            {
+                Console.WriteLine(ip);
+            }
+
+            string hostName = Dns.GetHostName();
+            Console.WriteLine(hostName);
+            IPHostEntry localhost = Dns.GetHostEntry(hostName);
+            foreach (IPAddress ip in localhost.AddressList)
+            {
+                Console.WriteLine("로컬IP =" + ip);
+            }
+        }
+    }
+}
