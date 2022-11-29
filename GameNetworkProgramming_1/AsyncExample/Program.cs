879a86d29fa14c115460689b3d1088109098780b
vmion <ohj106992@gmail.com>
2022-11-29 21:32:55 +0900

    _11_29_
    chatting_Server

---
+++ GameNetworkProgramming_1/AsyncExample/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+namespace AsyncExample
+{
+    public class User
+    {
+        public Socket sock;
+        public byte[] rBuffer;
+        public byte[] sBuffer;        
+        public User(Socket _sock)
+        {
+            sock = _sock;
+            sBuffer = new byte[128];
+            rBuffer = new byte[128];
+        } 
+    }
+    class Program
+    {
+        static string strIp = "218.234.62.112";
+        static int port = 8082;
+        static Socket listenSock;        
+        static Thread t1;
+        static List<User> userList;
+        static void Main(string[] args)
+        {
+            listenSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipEndpoint = new IPEndPoint(IPAddress.Parse(strIp), port);
+            userList = new List<User>();
+            listenSock.Bind(ipEndpoint);
+            Console.WriteLine("Bind");
+
+            ThreadStart threadStart = new ThreadStart(NewClient);
+            t1 = new Thread(threadStart);
+            t1.Start();
+
+        }
+        static void NewClient()
+        {
+            //사용자 접속
+            while (true)
+            {
+                listenSock.Listen(1000);                                                        
+                listenSock.BeginAccept(AcceptCallBack, null);                
+                Thread.Sleep(10);
+            }
+        }
+        static void AcceptCallBack(IAsyncResult ar)
+        {
+            Socket userSock = listenSock.EndAccept(ar);
+            User user = new User(userSock);
+            userList.Add(user);
+            byte[] tmp = Encoding.Default.GetBytes("Game에 오신 것을 환영합니다.");
+            userSock.Send(tmp);
+            userSock.BeginReceive(user.rBuffer, 0, user.rBuffer.Length,
+                SocketFlags.None, ReceiveCallBack, user);
+        }
+        static void ReceiveCallBack(IAsyncResult ar)
+        {
+            //userSock이 보낸 데이터를 다른 사용자에게 전송
+            User user = (User)ar.AsyncState; 
+            //자신의 메시지를 다른사용자에게 보내기 위하여 rBuffer의 데이터를 sBuffer에 전송            
+            for(int i = 0; i < userList.Count; i++)
+            {
+                Array.Copy(user.rBuffer, userList[i].sBuffer, user.rBuffer.Length);
+                userList[i].sock.BeginSend(userList[i].sBuffer, 0, userList[i].sBuffer.Length,
+                                        SocketFlags.None, SendCallBack, userList[i]);
+            }
+            Array.Clear(user.rBuffer, 0, user.rBuffer.Length);            
+        }
+        static void SendCallBack(IAsyncResult ar)
+        {
+            User user = (User)ar.AsyncState;
+            Array.Clear(user.sBuffer, 0, user.sBuffer.Length);
+            user.sock.BeginReceive(user.rBuffer, 0, user.rBuffer.Length,
+                                    SocketFlags.None, ReceiveCallBack, user);                       
+        }
+    }
+}
---
+++ GameNetworkProgramming_1/AsyncExample_Client/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace AsyncExample_Client
+{
+    public class User
+    {
+        public Socket sock;
+        public byte[] rBuffer;
+        public byte[] sBuffer;
+        public User(Socket _sock)
+        {
+            sock = _sock;
+            sBuffer = new byte[128];
+            rBuffer = new byte[128];
+        }
+    }
+    class Program
+    {
+        static Socket sock;
+        static int port = 8082;
+        static string strip = "218.234.62.112";
+        static User user;
+        static void Main(string[] args)
+        {            
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            user = new User(sock);
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse(strip), port);
+            sock.Connect(ip);
+            //byte[] receiveBuffer = new byte[128];
+            //byte[] sendBuffer = new byte[128];
+            byte[] tmp = new byte[128];
+            sock.Receive(tmp);
+            string receiveData = Encoding.Default.GetString(tmp);
+            Console.WriteLine(receiveData);
+            string line = string.Empty;
+            receiveData = string.Empty;
+            sock.BeginReceive(user.rBuffer, 0, user.rBuffer.Length,
+                                    SocketFlags.None, ReceiveCallBack, user);
+            while ((line = Console.ReadLine()) != null)
+            {
+                byte[] sends = Encoding.Default.GetBytes(line);
+                Array.Copy(sends, user.sBuffer, sends.Length);
+                user.sock.BeginSend(user.sBuffer, 0, user.sBuffer.Length,
+                                    SocketFlags.None, SendCallBack, sock);
+            }
+        }
+        static void ReceiveCallBack(IAsyncResult ar)
+        {
+            User user = (User)ar.AsyncState;
+            string receiveMessage = Encoding.Default.GetString(user.rBuffer);
+            Console.WriteLine(receiveMessage);
+            Array.Clear(user.rBuffer, 0, user.rBuffer.Length);            
+            user.sock.BeginReceive(user.sBuffer, 0, user.sBuffer.Length,
+                                    SocketFlags.None, ReceiveCallBack, user);
+        }
+        static void SendCallBack(IAsyncResult ar)
+        {
+            //User user = (User)ar.AsyncState;
+            Array.Clear(user.sBuffer, 0, user.sBuffer.Length);
+            //user.sock.BeginSend(user.rBuffer, 0, user.rBuffer.Length,
+                                    //SocketFlags.None, SendCallBack, user);
+        }
+    }
+}
